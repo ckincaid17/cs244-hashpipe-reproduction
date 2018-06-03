a929789a2ae41c5875dcf1f069b3e9f44084c7ff
Sierra Kaplan-Nelson <sierrakn@stanford.edu>
2018-06-03 11:32:36 -0700

    removed tutorials again

--- p4-old/env.sh
+++ p4-old/env.sh
@@ -1,7 +1,7 @@
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # ---------------- EDIT THIS ------------------
-BMV2_PATH=$THIS_DIR/../../behavioral-model
+BMV2_PATH=$THIS_DIR/../../bmv2
 # e.g. BMV2_PATH=$THIS_DIR/../bmv2
 P4C_BM_PATH=$THIS_DIR/../../p4c-bm
 # e.g P4C_BM_PATH=$THIS_DIR/../p4c-bm
