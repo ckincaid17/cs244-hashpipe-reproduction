c1a0e4ea529df6a6e8cad8fc8cb464068019edb0
sierrakn <sierrakn@stanford.edu>
2018-06-08 14:59:03 +0000

    figure 2 dc

--- evaluation/results.py
+++ evaluation/results.py
@@ -9,12 +9,11 @@ from flowCounter import FlowCounter
 from hashPipeSimulator import Simulator
 
 inputFile = 'data/header_fields_DC.csv'
-configs = [(140, 3360), (210, 5040), (350, 6720), (420, 8400)]
+# configs = [(140, 3360), (210, 5040), (350, 6720), (420, 8400)]
+configs = [(40, 840)]
 
 dVals = range(2, 9)
 mVals = [840 * i for i in range(1, 6)]
-
-# 840, 40 (m, k) for datacenter 
 
 trueCounter = FlowCounter(inputFile)
 totalFlows = trueCounter.getNumFlows()
@@ -62,31 +61,31 @@ def main():
   print ("Total flows: %d" % totalFlows)
 
   # Figure 2, false negatives
-  # print ("Finding false negatives")
-  # colors = ['r', 'b', 'k', 'm']
-  # shapes = ['s', 'o', 'D', '*']
-  # for i, (k, m) in enumerate(configs):
-  #   plt.plot(dVals, simulateOneConfiguration(k, m), color=colors[i], marker=shapes[i])
+  print ("Finding false negatives")
+  colors = ['r', 'b', 'k', 'm']
+  shapes = ['s', 'o', 'D', '*']
+  for i, (k, m) in enumerate(configs):
+    plt.plot(dVals, simulateOneConfiguration(k, m), color=colors[i], marker=shapes[i])
 
-  # plt.ylabel('False Negative %')
-  # plt.xlabel('Number of table stages (d)')
-  # plt.legend(['k = %d, m = %d' % (k, m) for (k, m) in configs], loc='best')
+  plt.ylabel('False Negative %')
+  plt.xlabel('Number of table stages (d)')
+  plt.legend(['k = %d, m = %d' % (k, m) for (k, m) in configs], loc='best')
 
-  # plt.savefig('figure_2_DC.png')
-  # plt.close()
+  plt.savefig('figure_2_DC.png')
+  plt.close()
 
   # Figure 3, duplicates
-  print ("Finding duplicates")
-  colors = ['r', 'k', 'c']
-  linestyles = ['--', '-.', ':']
-  for i, d in enumerate([8, 4, 2]):
-    plt.plot([(m / 840 * 15)/10.0 for m in mVals], findDupPercentage(d), color=colors[i], ls=linestyles[i])
+  # print ("Finding duplicates")
+  # colors = ['r', 'k', 'c']
+  # linestyles = ['--', '-.', ':']
+  # for i, d in enumerate([8, 4, 2]):
+  #   plt.plot([(m / 840 * 15)/10.0 for m in mVals], findDupPercentage(d), color=colors[i], ls=linestyles[i])
 
-  plt.ylabel('Duplicate Entries %')
-  plt.xlabel('Memory (in KB)')
-  plt.legend(['d = %d' % d for d in [8, 4, 2]], loc='best')
+  # plt.ylabel('Duplicate Entries %')
+  # plt.xlabel('Memory (in KB)')
+  # plt.legend(['d = %d' % d for d in [8, 4, 2]], loc='best')
 
-  plt.savefig('figure_3_DC.png')
+  # plt.savefig('figure_3_DC.png')
 
 if __name__ == '__main__':
   main()
